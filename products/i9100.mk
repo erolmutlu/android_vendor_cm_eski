8e6c37c3e66f26c5c90ced897105956cf6298c47
Erol Mutlu <cm@erolmutlu.com>
2012-10-16 10:03:11 +0200

    Revert "change vendor config"
    This reverts commit 732ace9a2b370bfd620aef700041e06abdf77b17.

--- products/i9100.mk
+++ products/i9100.mk
@@ -2,12 +2,12 @@
 $(call inherit-product, device/samsung/i9100/full_i9100.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/cm/configs/common.mk)
+$(call inherit-product, vendor/supernexus/configs/common.mk)
 
 # Inherit GSM files.
-$(call inherit-product, vendor/cm/configs/gsm.mk)
+$(call inherit-product, vendor/supernexus/configs/gsm.mk)
 # Setup device specific product configuration.
-PRODUCT_NAME := jb_i9100
+PRODUCT_NAME := cm_i9100
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := i9100
 PRODUCT_MODEL := GT-I9100
@@ -16,11 +16,11 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=GT-I9100 TARGET_DEVICE=GT-I9100 BUILD_FINGERPRINT=samsung/GT-I9100/GT-I9100:4.0.3/IML74K/XXLPQ:user/release-keys PRIVATE_BUILD_DESC="GT-I9100-user 4.0.3 IML74K XXLPQ release-keys"
 
 # Inherit common build.prop overrides
--include vendor/cm/configs/common_versions.mk
+-include vendor/supernexus/configs/common_versions.mk
 
 # Copy GalaxyS3 specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-    vendor/cm/prebuilt/common/bootanimation/480.zip:system/media/bootanimation.zip 
+    vendor/supernexus/prebuilt/bootanimations/BOOTANIMATION-480x800.zip:system/media/bootanimation.zip 
 
 # Inherit drm blobs
--include vendor/cm/configs/common_drm_phone.mk
+-include vendor/supernexus/configs/common_drm_phone.mk
