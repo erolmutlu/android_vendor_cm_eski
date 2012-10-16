d85be2d6f4b097a51530943f728fc4602059c908
Erol Mutlu <cm@erolmutlu.com>
2012-10-16 10:02:54 +0200

    Revert "change vendor config"
    This reverts commit 3627781726c23bebad8706c49b52bf55654cb386.

--- config/common_versions.mk
+++
@@ -1,8 +0,0 @@
-# Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03R BUILD_ID=JRO03R BUILD_VERSION_TAGS=release-keys BUILD_UTC_DATE=$(shell date +"%s")
-
-DATE = $(shell date +%Y%m%d)
-
-# Rom Manager properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sn.version=$(TARGET_PRODUCT)-$(DATE)
